9c25cd77c253c328b12943e42ac0615a708ed645
Shidong Lin <linshidongbqwl@gmail.com>
2019-08-19 22:13:39 -0700

    init

---
+++ DeezerSwift/View Controllers/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  DeezerSwift
+//
+//  Created by Shidong Lin on 8/19/19.
+//  Copyright © 2019 Shidong Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UITabBarController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        viewControllers = [
+
+        ]
+    }
+
+    @available(*, unavailable, message: "Not implemented")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ DeezerSwift/View Controllers/SearchSingerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchSingerViewController.swift
+//  DeezerSwift
+//
+//  Created by Shidong Lin on 8/19/19.
+//  Copyright © 2019 Shidong Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchSingerViewController: UIViewController {
+    let searchBar = UISearchBar()
+
+}
